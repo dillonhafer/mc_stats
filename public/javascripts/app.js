5ab3d4510e5caa56fbaeac85d85eb3bbf4fb7934
Dillon Hafer <dh@dillonhafer.com>
2015-02-02 17:31:40 -0500

    Adds basic js rendering

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,55 @@
-$(document).on('click', '.f-dropdown a', function(e) {
-  e.preventDefault();
-  $(this).parent().parent().css('left', '-9999999px')
-  $.post(this.getAttribute('href'));
-})
-$(document).foundation();
+(function() {
+  function setNames() {
+    $.get("/players", function(players) {
+      for(var i in players) {
+        player = players[i]
+        var dom_id = player.uuid
+        $('#uuid-'+dom_id).html(player.name)
+      }
+    })
+  }
+
+  function createTitle(uuid) {
+    title = document.createElement('li')
+    title.setAttribute('class', 'title')
+    title.setAttribute('id', 'uuid-'+ uuid)
+    return title
+  }
+
+  function createItem(label, value) {
+    var name = label.split('.').slice(-1)[0].replace(/-/g, ' ')
+    li = document.createElement('li')
+    li.setAttribute('data-'+label, value)
+    li.setAttribute('class', 'bullet-item')
+    li.innerHTML = name+": "+value
+    return li
+  }
+
+  function createPlayer(player) {
+    var li = document.createElement('li')
+    var ul = document.createElement('ul')
+    ul.setAttribute('class', 'pricing-table')
+    playerTitle = createTitle(player.UUID)
+    ul.appendChild(playerTitle)
+
+    for (var key in player.data) {
+      if (player.data.hasOwnProperty(key)) {
+        item = createItem(key, player.data[key])
+        ul.appendChild(item)
+      }
+    }
+
+    li.appendChild(ul)
+    return li
+  }
+
+  $(document).foundation();
+
+  $.get("/stats", function(players) {
+    for(var i in players) {
+      player = createPlayer(players[i]);
+      $('.main').append(player)
+    }
+    setNames()
+  })
+}).call(this)
