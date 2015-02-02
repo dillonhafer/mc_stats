2da9c7da4a162619a04a567d17589923903923a3
Dillon Hafer <dh@dillonhafer.com>
2015-02-02 15:28:41 -0500

    Adds json API

--- main.go
+++ main.go
@@ -1,33 +1,64 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
-func readStats(string dir) {
-	files, err := ioutil.ReadDir(dir)
-	fmt.Println(files)
-	for err, f := range files {
-		fmt.Println(f.Name())
-		player, err := ioutil.ReadFile(f.Name())
-		fmt.Println(player)
+func statsDirExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func readStats(dir string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Command Must be a GET\n")
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		files, _ := ioutil.ReadDir(dir)
+		players := make([]string, 0, len(files))
+
+		for _, file := range files {
+			path := filepath.Join(dir, file.Name())
+			f, _ := ioutil.ReadFile(path)
+			players = append(players, string(f))
+		}
+
+		player_json := strings.Join(players, ",")
+		fmt.Fprint(w, player_json)
 	}
 }
 
 func main() {
+	var dir string
 	staticFiles := http.FileServer(http.Dir("public"))
 	flag.StringVar(&dir, "dir", "", "path to Minecraft world")
 	flag.Parse()
 
+	properStatsDir, _ := statsDirExists(dir)
+	if !properStatsDir {
+		fmt.Fprintln(os.Stderr, "You must provide a directory the world.")
+		fmt.Println("Run `mc_stats -h` for more startup options")
+		return
+	}
+
 	http.Handle("/", staticFiles)
-	http.Handle("/stats", readStats(dir))
+	http.HandleFunc("/stats", readStats(dir))
 
 	fmt.Println("Server running and listening on port 22334")
 	fmt.Println("Run `mc_stats -h` for more startup options")
