77c9cdbe5176a9cd1cd61ee5b975a15bd2050ae1
Dillon Hafer <dh@dillonhafer.com>
2017-11-18 09:31:51 -0600

    Make deploy script prettier

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,38 @@
 set -e
 
 source .env.deploy
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-echo 'Building server'
+function notice() {
+  printf "${GREEN}---->${NC} $1\n"
+}
+
+echo "Starting Full Deploy to $server"
+notice 'Building server'
 
 GOOS=linux GOARCH=386 go build
 
-echo 'Compressing build'
+notice 'Compressing build'
 tar czf mc_stats.tar.gz mc_stats
 
-echo 'Builing frontend'
-cd frontend && yarn build && cd ..
+notice 'Builing frontend'
+cd frontend && yarn build > /dev/null && cd ..
 
-echo 'Compressing frontend'
+notice 'Compressing frontend'
 tar czf frontend.tar.gz frontend/build
 
-echo 'Uploading to server'
-scp mc_stats.tar.gz frontend.tar.gz $server:$deploy_dir
+notice 'Uploading to server'
+scp -q mc_stats.tar.gz frontend.tar.gz $server:$deploy_dir
 
-echo 'Installing server'
+notice 'Installing server'
 ssh $server "cd $deploy_dir && tar xzf mc_stats.tar.gz && tar xzf frontend.tar.gz && rm mc_stats.tar.gz && rm frontend.tar.gz"
 
-echo 'Cleaning up'
+notice 'Cleaning up'
 rm mc_stats.tar.gz frontend.tar.gz
 
-echo 'Restarting server'
+notice 'Restarting server'
 ssh $server 'systemctl restart mc-stats.service'
+
+echo '      ✨  Done.'
